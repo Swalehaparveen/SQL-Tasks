76f00b042174a8f71915929e9fe3e59f32af6558
Swalehaparveen <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-08 16:53:00 +0530

    Task2.sql

--- All Disabled Config/Task2.sql
+++ All Disabled Config/Task2.sql
@@ -1,1 +1,17 @@
+SELECT
+    `catalog_product_entity`.`sku` AS `sku`,
+    `catalog_product_entity`.`type_id` AS `Product Type`,
+    `catalog_product_entity_int`.`value` AS `Status`
+FROM `eav_attribute` 
+JOIN `catalog_product_entity_int`
+ON`eav_attribute`.`attribute_id` = `catalog_product_entity_int`.`attribute_id`
+         
+JOIN `catalog_product_entity`
+ON `catalog_product_entity_int`.`entity_id` = `catalog_product_entity`.`entity_id`
 
+WHERE
+    (
+	`eav_attribute`.`attribute_code` = 'status' AND
+    `catalog_product_entity_int`.`value` = 2 AND
+    `catalog_product_entity`.`type_id` = 'configurable'
+    );
