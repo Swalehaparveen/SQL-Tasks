f86c39ed02571e8c69d1ab0bf84f7357a8c3954a
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-13 23:28:38 +0530

    Update Task3.sql

--- SKUs from specific Category/Task3.sql
+++ SKUs from specific Category/Task3.sql
@@ -1,1 +1,13 @@
-
+SELECT
+    p.entity_id,
+    p.sku
+FROM
+    catalog_product_entity AS p
+JOIN catalog_category_product AS cp
+ON
+    p.entity_id = cp.product_id
+JOIN catalog_category_entity AS c
+ON
+    cp.category_id = c.entity_id
+WHERE
+    c.entity_id = '806';
