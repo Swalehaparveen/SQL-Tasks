79864e7606852323095643eca4163caed6508b70
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-15 12:58:57 +0530

    Update Task5.sql

--- Order increment ID & All ordered SKUs in one column/Task5.sql
+++ Order increment ID & All ordered SKUs in one column/Task5.sql
@@ -1,1 +1,9 @@
-
+SELECT 
+    o.entity_id,
+    GROUP_CONCAT(od.sku) AS OrderedSKUs
+FROM 
+    sales_order o
+JOIN 
+    sales_order_item od ON o.entity_id = od.order_id
+GROUP BY 
+   o.increment_id;
