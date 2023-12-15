9bba46e312d910602dae9b2e157da90d5b48de4f
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-15 14:38:04 +0530

    Update Task5.sql

--- Order which have at least one product that is now disabled/Task5.sql
+++ Order which have at least one product that is now disabled/Task5.sql
@@ -1,1 +1,16 @@
-
+SELECT DISTINCT 
+    o.entity_id,
+    o.increment_id,
+    GROUP_CONCAT(od.sku) AS OrderedSKUs,
+    cpei.value AS 'is_enabled'
+FROM 
+    sales_order o
+JOIN 
+    sales_order_item od ON o.entity_id = od.order_id
+JOIN 
+    catalog_product_entity p ON od.sku = p.sku
+LEFT JOIN catalog_product_entity_int cpei ON (p.entity_id=cpei.entity_id
+                                              AND cpei.attribute_id=97)
+WHERE 
+    cpei.value = 2
+GROUP BY o.entity_id;
