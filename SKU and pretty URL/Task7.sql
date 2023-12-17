59a8de8ca474c68452db5b4b8cf94c4e01a06a01
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-17 16:12:52 +0530

    Update Task7.sql

--- SKU and pretty URL/Task7.sql
+++ SKU and pretty URL/Task7.sql
@@ -1,1 +1,15 @@
-
+SELECT
+    p.entity_id,
+    cpev.value AS URL
+FROM
+    catalog_product_entity p
+JOIN catalog_product_entity_varchar cpev ON
+    (
+        p.entity_id = cpev.entity_id AND cpev.attribute_id = 119
+    )
+JOIN catalog_product_entity_int cpei ON
+    (
+        p.entity_id = cpei.entity_id AND cpei.attribute_id = 99
+    )
+WHERE
+    cpei.attribute_id = 99 AND cpei.value = 4;
