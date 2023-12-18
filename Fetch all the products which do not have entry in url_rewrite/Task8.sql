92d2e9cf0fb5ed1fc69ed724a9ddf216877bb051
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-18 12:44:07 +0530

    Update Task8.sql

--- Fetch all the products which do not have entry in url_rewrite/Task8.sql
+++ Fetch all the products which do not have entry in url_rewrite/Task8.sql
@@ -1,1 +1,10 @@
-
+SELECT
+    e.sku AS SKU,
+    u.request_path AS URL
+FROM
+    catalog_product_entity AS e
+LEFT JOIN url_rewrite AS u
+ON
+    u.entity_id = e.entity_id AND u.entity_type = 'product' 
+WHERE
+    u.request_path IS NULL;
