29ea1fd3eb6cc9abda826a97d6534809021ba5fc
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-18 17:17:23 +0530

    Update Task10.sql

--- All the orders which have more that 2 items/Task10.sql
+++ All the orders which have more that 2 items/Task10.sql
@@ -1,1 +1,11 @@
-
+SELECT 
+   o.increment_id AS IncrementID,
+    GROUP_CONCAT(od.sku) AS OrderedSKUs,
+    o.total_qty_ordered
+FROM 
+    sales_order o
+JOIN 
+    sales_order_item od ON o.entity_id = od.order_id
+WHERE o.total_qty_ordered >= 2
+GROUP BY 
+   o.increment_id;
