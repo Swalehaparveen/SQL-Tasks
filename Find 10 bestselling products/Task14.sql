9c930692f051ae9ab3b40b3e6fe3d9896c9b18a6
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-19 17:43:43 +0530

    Update Task14.sql

--- Find 10 bestselling products/Task14.sql
+++ Find 10 bestselling products/Task14.sql
@@ -1,1 +1,18 @@
-
+SELECT
+    oi.product_id,
+    SUM(oi.qty_ordered) AS total_quantity_ordered,
+    pr.sku
+    
+FROM
+    sales_order_item AS oi
+JOIN catalog_product_entity AS pr
+ON
+    oi.product_id = pr.entity_id
+WHERE
+    oi.parent_item_id IS NULL
+GROUP BY
+    oi.product_id
+ORDER BY
+    total_quantity_ordered
+DESC
+LIMIT 10;
