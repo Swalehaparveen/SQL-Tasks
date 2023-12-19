b9b9af78a428ab46d264b48dc2b80ff5b8679afd
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-19 10:52:54 +0530

    Update Task12.sql

--- Create a backup of catalog_product_entity/Task12.sql
+++ Create a backup of catalog_product_entity/Task12.sql
@@ -1,1 +1,5 @@
+-- Create a backup table with the same structure as catalog_product_entity
+CREATE TABLE catalog_product_entity_backup LIKE catalog_product_entity;
 
+-- Copy data from catalog_product_entity to catalog_product_entity_backup
+INSERT INTO catalog_product_entity_backup SELECT * FROM catalog_product_entity;
