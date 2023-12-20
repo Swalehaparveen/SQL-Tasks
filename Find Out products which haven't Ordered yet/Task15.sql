2e629053bdef8d468e76cbbd13a04b77f7628808
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-20 15:24:27 +0530

    Update Task15.sql

--- Find Out products which haven't Ordered yet/Task15.sql
+++ Find Out products which haven't Ordered yet/Task15.sql
@@ -1,1 +1,16 @@
-
+SELECT
+    p.sku,
+    p.created_at,
+    soi.item_id
+FROM
+    catalog_product_entity AS p
+JOIN sales_order_item AS soi
+ON
+    soi.item_id = p.entity_id
+WHERE
+    p.created_at < DATE_SUB(NOW(), INTERVAL 6 MONTH) AND soi.item_id NOT IN(
+    SELECT
+        soi.item_id
+    FROM
+        sales_order_item
+);
