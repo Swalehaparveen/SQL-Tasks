957294e2d82f92f16e6337ffcbd77031fe18b693
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-20 18:07:07 +0530

    Update Task16.sql

--- Top 5 customers based on num of orders/Task16.sql
+++ Top 5 customers based on num of orders/Task16.sql
@@ -1,1 +1,17 @@
-
+SELECT
+    c.entity_id AS customer_id,
+    c.email AS customer_email,
+    COUNT(o.entity_id) AS total_orders
+FROM
+    customer_entity c
+LEFT JOIN sales_order o ON
+    c.entity_id = o.customer_id
+WHERE
+    o.state <> 'canceled' AND c.entity_id IS NOT NULL
+GROUP BY
+    c.entity_id,
+    c.email
+ORDER BY
+    total_orders
+DESC
+LIMIT 5;
