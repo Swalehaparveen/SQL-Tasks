8bb21a62e98ed7a25c41863cb601f984944a9f99
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2024-01-02 11:43:40 +0530

    Update Task1.sql

--- Config&SimpleSKUs/Task1.sql
+++ Config&SimpleSKUs/Task1.sql
@@ -2,7 +2,7 @@ SELECT
     parent.type_id AS parent_product_type,
     parent.sku AS parent_sk,
     child.type_id AS child_product_type,
-    child.sku child_sku
+    child.sku AS child_sku
 FROM
     catalog_product_entity AS parent
 JOIN catalog_product_relation AS link
@@ -11,3 +11,6 @@ ON
 JOIN catalog_product_entity AS child
 ON
     child.entity_id = link.child_id
+WHERE
+    ((child.type_id = 'simple' OR child.type_id = 'configurable') OR (parent.type_id = 'simple' OR parent.type_id = 'configurable'))
+    AND (child.type_id != 'bundle' AND parent.type_id != 'bundle');
